79f07c1fb432c2cdb0ed255708e1960aa445374d
pjaworski <pawel.jaworski@altkomsoftware.pl>
2019-10-02 20:57:48 +0200

    istio setup for new services

--- build-app.sh
+++ build-app.sh
@@ -4,5 +4,7 @@ source ~/.nvm/nvm.sh
 nvm use v10.15.3
 npm install --prefix ./offer/offer-frontend/
 npm run build --prefix ./offer/offer-frontend/
+npm install --prefix ./loan/loan-frontend/
+npm run build --prefix ./loan/loan-frontend/
 
 mvn clean install
--- istio/setup-app.sh
+++ istio/setup-app.sh
@@ -7,7 +7,12 @@ kubectl replace -f 3_routing.yaml --force
 kubectl label namespace app istio-injection=enabled --overwrite
 
 mvn clean install dockerfile:build -Pdocker -f ../user/user-micronaut-app/pom.xml
+mvn clean install dockerfile:build -Pdocker -f ../product/product-micronaut-app/pom.xml
 mvn clean install dockerfile:build -Pdocker -f ../offer/offer-spring-app/pom.xml
+mvn clean install dockerfile:build -Pdocker -f ../instalment/instalment-spring-app/pom.xml
+mvn clean install dockerfile:build -Pdocker -f ../loan/loan-spring-app/pom.xml
+
 docker build -t javorex/offer-frontend ../offer/offer-frontend/
+docker build -t javorex/loan-frontend ../loan/loan-frontend/
 
 kubectl replace -f 4_deploy.yaml --force
